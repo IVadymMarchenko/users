8819e2a27a8c82a7d63b364a1c08d6e72fcd182f
IVadymMarchenko <142535938+IVadymMarchenko@users.noreply.github.com>
2023-10-21 22:54:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from datetime import date, datetime,timedelta
 from datetime import datetime, timedelta, date
 
 def get_birthdays_per_week(users):
-    days = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [],'Saturday':[],'Sunday':[]}
+    days = {"Monday": [], "Tuesday": [], "Wednesday": [], "Thursday": [], "Friday": [], 'Saturday': [], 'Sunday': []}
     start_data = date.today()
-    end_data=date.today()+timedelta(days=7)
+    end_data = date.today() + timedelta(days=7)
     while start_data != end_data:
         for i in users:
             day = start_data.strftime('%A')
             start_data_ = start_data.strftime('%m-%d').split('-')
             end_data_ = i["birthday"].strftime('%m-%d').split('-')
-            if start_data_==end_data_ and i['name'] not in days[day] and day!='Sunday' and day!='Saturday':
-                days.get(day,[]).append(i['name'])
-            if start_data_==end_data_ and i['name'] not in days[day]:
-                days.get('Monday',[]).append(i['name'])
-        start_data+=timedelta(days=1)
+            if start_data_ == end_data_ and i['name'] not in days[day] and day != 'Sunday' and day != 'Saturday':
+                days.get(day, []).append(i['name'])
+            if start_data_ == end_data_ and i['name'] not in days[day]:
+                days.get('Monday', []).append(i['name'])
+        start_data += timedelta(days=1)
     days.pop('Saturday')
     days.pop('Sunday')
     users = {i: j for i, j in days.items() if j}
